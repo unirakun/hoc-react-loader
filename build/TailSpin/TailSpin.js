957eca931e61979cb8cb26419a4f950e51c861da
Fabien JUIF <fabien.juif@zenika.com>
2016-10-07 14:48:58 +0200

    rebuild & publish

--- build/TailSpin/TailSpin.js
+++ build/TailSpin/TailSpin.js
@@ -24,6 +24,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+/* global window */
 var getBackgroundColor = function getBackgroundColor(node) {
   return window.getComputedStyle(node, null).getPropertyValue('background-color');
 };
