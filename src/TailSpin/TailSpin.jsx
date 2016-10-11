314078a78644bcd3944c7641a2241cdc46a9b333
Fabien JUIF <fabien.juif@zenika.com>
2016-10-11 19:31:25 +0200

    Add TailSpin tests and fix it

--- src/TailSpin/TailSpin.jsx
+++ src/TailSpin/TailSpin.jsx
@@ -22,14 +22,14 @@ class TailSpin extends Component {
 
   setColor = () => {
     let parent = this.svg.parentNode
-    let parentColor = parent ? getBackgroundColor(parent) : ''
+    let parentColor = parent ? getBackgroundColor(parent) : undefined
 
-    while (parent && (parentColor && parentColor === '')) {
+    while (parent && !parentColor) {
       parent = parent.parentNode
-      parentColor = getBackgroundColor(parent)
+      if (parent) parentColor = getBackgroundColor(parent)
     }
 
-    if (parentColor && parentColor !== '') {
+    if (parentColor) {
       const tinyC = tinycolor(parentColor)
       const color = tinyC.isDark() ? tinyC.lighten(20) : tinyC.darken(20)
 
