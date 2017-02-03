77233942a8b5c626abecd1d3649eb997c64a2377
Matt Polichette <matt.polichette@spingo.com>
2017-02-03 11:06:28 -0700

    Feature: Pass className and style in default Loader

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import core from './core'
 import TailSpin from './TailSpin'
 
+const defaultLoadingIndicator = ({ className, style }) =>
+  <TailSpin className={className} style={style} />
+
 export default (
   ComposedComponent,
   {
-    renderLoadingIndicator = (() => <TailSpin />),
+    renderLoadingIndicator = defaultLoadingIndicator,
     ...rest
   } = {},
 ) => core(ComposedComponent, { ...rest, renderLoadingIndicator })
