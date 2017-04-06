fb0060a567e3825a80fdc3e16957c7932541d8f1
Benjamin Cavy <benjamin.cavy@gmail.com>
2017-04-06 08:31:48 +0200

    Typo

--- examples/src/components/Examples/ErrorIndicator.jsx
+++ examples/src/components/Examples/ErrorIndicator.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import loader from 'hoc-react-loader'
 
-const EmptyComponent = () => <div /> // thsi will never be display
+const EmptyComponent = () => <div /> // this will never be display
 
 const ErrorIndicator = (props) => <div {...props}>An error occured !</div>
 
